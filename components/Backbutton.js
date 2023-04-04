57815f41dea48fba51b3e363be681969062927d6
Timimah <44163985+Timimah@users.noreply.github.com>
2023-04-04 16:11:59 +0100

    created backbutton

---
+++ components/Backbutton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
+
+export const BackButton = ({ onPress }) => (
+  <TouchableOpacity onPress={onPress} style={styles.button}>
+    <Image source={require('../assets/backarrow.png')} />
+  </TouchableOpacity>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    // backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#141414',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+});
