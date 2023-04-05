5cf5b773da015ed48f970c72ad9abc5acf1f8894
Timimah <44163985+Timimah@users.noreply.github.com>
2023-04-05 15:34:34 +0100

    created button component

--- components/Button.js
+++ components/Button.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
-export const Button = ({ onPress, title }) => (
-  <TouchableOpacity onPress={onPress} style={styles.button}>
-    <Text style={styles.buttonText}>{title}</Text>
+export const Button = ({ onPress, title, btnstyle, txtstyle }) => (
+  <TouchableOpacity onPress={onPress} style={btnstyle}>
+    <Text style={txtstyle}>{title}</Text>
   </TouchableOpacity>
 );
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#141414',
-    borderRadius: 8,
-    height: 54,
-    justifyContent: "center",
-    padding: 10,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
